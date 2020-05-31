7b836ef29500e53dbde9ebc053afb253d5e4fea0
BorryS <andrzej.kimel@gmail.com>
2020-05-31 10:53:45 +0200

    basic controls

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -23,21 +24,17 @@ public class MyGdxGame extends ApplicationAdapter {
   @Override
   public void render() {
     float time = Gdx.graphics.getDeltaTime();
-    float newX = x + speedX * time;
-    float newY = y + speedY * time;
-
-    if (newX + img.getWidth() >= Gdx.graphics.getWidth() || newX < 0) {
-      speedX *= -1;
-      newX = x + speedX * time;
-    }
-    if (newY + img.getHeight() >= Gdx.graphics.getHeight() || newY < 0) {
-      speedY *= -1;
-      newY = y + speedY * time;
+    if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+      x -= speedX * time;
+    } else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+      x += speedX * time;
     }
 
-    this.x = newX;
-    this.y = newY;
-
+    if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+      y += speedY * time;
+    } else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+      y -= speedY * time;
+    }
 
     Gdx.gl.glClearColor(0.9f, 0.9f, 0.9f, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
